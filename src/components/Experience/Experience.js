501f0f69640e62c34c4bf456f2f87a5848402729
Soran Haruti <haruti.soran@gmail.com>
2021-03-22 16:36:21 -0400

    updates in exp

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -13,18 +13,22 @@ const Experience = () => {
             <h3>2018 - Present</h3>
             <h3>Frontend Developer - EasyTech Co.</h3>
             <p>
-              ● Implement web design and development principles to build stable
-              software ● Bring mock-ups to life using HTML, CSS, JavaScript ●
-              Collaborate closely with the team to support projects during all
-              phases of delivery ● Build websites and apps that are responsive
-              and usable ● Monitor website performance and rectify
-              front-end-related issues ● Communicate technical issues in an
-              understandable manner to the team and clients ● Identify
-              innovative ideas and proof of concepts according to project
-              requirements ● Provide guidance and troubleshooting support to
-              clients ● Maintain an organized workflow using GitHub ● Test to
-              identify bugs and technical issues before and after deploying ●
-              Document bug reports, tickets, and any code changes
+              Implement web design and development principles to build stable
+              software Bring mock-ups to life using HTML, CSS, JavaScript as
+              well as React.js and React Native. Collaborate closely with the
+              team to support projects during all phases of delivery Build
+              websites and apps that are responsive and usable Monitor website
+              performance and rectify front-end-related issues Communicate
+              technical issues in an understandable manner to the team and
+              clients Identify innovative ideas and proof of concepts according
+              to project requirements Provide guidance and troubleshooting
+              support to clients Maintain an organized workflow using GitHub
+              Test to identify bugs and technical issues before and after
+              deploying Document bug reports, tickets, and any code changes
+              Environment: HTML, CSS, JAVASCRIPT, TYPESCRIPT, SASS, REACT JS,
+              REACT NATIVE,GIT,VERSION CONTROL,GITHUB, NODE.JS, WEB SERVICES
+              API, DOM, EXPRESS.JS, MONGODB, MYSQL, EXPO, HEROKU, AGILE
+              METHODOLOGIES
             </p>
           </div>
         </div>
@@ -35,12 +39,13 @@ const Experience = () => {
             <h3>2019 - 2020</h3>
             <h3>R&D Engineer - Advanced Upsteam</h3>
             <p>
-              ● Performed analyses and statistical comparisons of results to
-              contribute to technical and product innovation ● Compiled and
-              analyzed data from personal and team analyses ● Tested products,
-              compounds, or batches for defined metrics and contaminants ●
+              Performed analyses and statistical comparisons of results to
+              contribute to technical and product innovation Compiled and
+              analyzed data from personal and team analyses Tested products,
+              compounds, or batches for defined metrics and contaminants
               Maintained, operated, and calibrated laboratory equipment,
-              including necessary supplies
+              including necessary supplies Environment: Python, SolidWorks,
+              AutoCAD
             </p>
           </div>
         </div>
@@ -51,11 +56,19 @@ const Experience = () => {
             <h3>2014 - 2019</h3>
             <h3>Application - Baker Hughes</h3>
             <p>
-              ● Conducted direct business-to-business selling and professional
-              product detailing ● Achieved established revenue growth
-              expectations within assigned territory ● Prepared well schematics
-              ● Worked on commercial and technical proposals ● Worked on
-              purchase orders, quotes and invoices
+              Led the coding, testing, documentation, implementation and
+              maintenance of information technology solutions for business
+              processes and customer requirements Developed creative
+              proof-of-concepts and prototypes of easy to navigate user
+              interfaces (UI) to include graphics, icons and color schemes
+              Collaborated with teams to define standards, best practices and
+              processes around application development and design Engaged with
+              cross-functional teams and internal customers in a global-matrix
+              environment to deliver innovative software solutions Implemented
+              functional and technical enhancements to meet business needs and
+              requirements Environment: Angular, Node, JavaScript, TypeScript,
+              HTML5 and CSS, Object-Oriented-Programming, Git NoSQL database,
+              MongoDB RESTful API
             </p>
           </div>
         </div>
